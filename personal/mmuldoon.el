9158798992b2abcb5d55b9e57adf3efbd83d5c3f
Mickey Muldoon 🐢 <mmuldoon@blueorigin.com>
2021-01-26 14:44:02 -0800

    More emacs config.

--- personal/mmuldoon.el
+++ personal/mmuldoon.el
@@ -8,13 +8,14 @@
 (global-set-key (kbd "s-<backspace>") 'crux-kill-line-backwards)
 (global-set-key (kbd "M-s-<backspace>") 'kill-line)
 (global-set-key (kbd "M-<f5>") 'org-capture)
-;; (global-set-key (kbd "M-<f7>") '+org-search)
 ;; (global-set-key (kbd "M-<f5>") 'org-roam-capture)
 ;; (global-set-key (kbd "<f5>") 'mmuldoon/org-file)
 ;; (global-set-key (kbd "M-<f7>") 'org-roam-insert)
 ;; (global-set-key (kbd "M-<f5>") 'org-roam-insert-immediate)
 (global-set-key (kbd "<f5>") 'org-roam-find-file)
-(prelude-require-packages '(indium ng2-mode clj-refactor discover-clj-refactor org-roam org-download company-org-roam flycheck-clj-kondo org-drill))
+(add-to-list 'load-path "~/src/org-fc/")
+(prelude-require-packages '(indium ng2-mode clj-refactor discover-clj-refactor org-roam org-download flycheck-clj-kondo org-drill groovy-mode))
+
 (setq prelude-format-on-save t)
 (add-hook 'js2-mode-hook #'(lambda() (setq js-indent-level 2)))
 (add-hook 'json-mode-hook #'flycheck-mode)
@@ -25,7 +26,7 @@
 (add-hook 'org-mode-hook 'org-download-enable)
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 
-(setq user-full-name "Mickey Muldoon")
+(setq user-full-name "mickey muldoon")
 (setq user-mail-address "muldoon.mickey@gmail.com")
 
 (setq initial-scratch-message nil)
@@ -38,8 +39,8 @@
 
 (setq org-drill-scope 'directory)
 
-;; https://github.com/jrblevin/config/blob/master/.emacs.d/init.el#L1301
-;; Store backup files in one place.  Do the same for auto save files.
+;; https://github.com/jrblevin/config/blob/master/.emacs.d/init.el#l1301
+;; store backup files in one place.  do the same for auto save files.
 (defvar mm-backup-directory (expand-file-name "backups/" user-emacs-directory))
 (unless (file-exists-p mm-backup-directory)
   (make-directory mm-backup-directory t))
@@ -56,7 +57,7 @@
       auto-save-list-file-prefix mm-backup-directory)
 
 
-;;; Export org files into their own directories.
+;;; export org files into their own directories.
 (defun org-export-output-file-name-modified (orig-fun extension &optional subtreep pub-dir)
   (unless pub-dir
     (setq pub-dir "exported-org-files")
@@ -64,13 +65,14 @@
       (make-directory pub-dir)))
   (apply orig-fun extension subtreep pub-dir nil))
 
-;; Remove for now since it gets in the way of plantuml embeds.
+;; remove for now since it gets in the way of plantuml embeds.
 ;; (advice-add 'org-export-output-file-name :around #'org-export-output-file-name-modified)
 
 (setq-default org-download-image-dir "./_resources")
 (require 'org-drill)
 
-(setq org-roam-directory "~/org")
+(setq org-roam-directory (file-truename "~/org"))
+(setq org-roam-completion-everywhere t)
 (setq org-roam-title-sources '((title) alias))
 (setq mmuldoon/agenda-subdirs '("/private-notes/" "/todo/" "/mmuldoon-work/"))
 (setq org-agenda-files
@@ -97,7 +99,7 @@
 (add-to-list 'org-structure-template-alist
              '("j" .  "src clojure"))
 
-(setq org-plantuml-jar-path (expand-file-name "/usr/local/Cellar/plantuml/1.2020.19/libexec/plantuml.jar"))
+(setq org-plantuml-jar-path (expand-file-name "/usr/local/cellar/plantuml/1.2020.19/libexec/plantuml.jar"))
 
 (defun +org-search ()
   (interactive)
@@ -108,22 +110,31 @@
   (crux-cleanup-buffer-or-region)
   (fixup-whitespace))
 
-;;; Require
+(defun +mm-focus-mode ()
+  (interactive)
+  (nlinum-mode -1)
+  (visual-line-mode 1)
+  (whitespace-mode -1))
+
+
+(add-hook 'writeroom-mode-hook '+mm-focus-mode)
+
+;;; require
 
 (eval-and-compile (require 'org)) ; for `org-completing-read'
 
-;;; Code:
+;;; code:
 
 (defvar ocv/capture-prmt-history nil
-  "History of prompt answers for Org capture.")
+  "history of prompt answers for org capture.")
 (defvar ocv/capture-prmt-type-history nil
-  "History of prompt answers for Org capture.")
+  "history of prompt answers for org capture.")
 
 (defun ocv/prmt (prompt variable &optional default history)
-  "PROMPT for string, save it to VARIABLE and insert it.
-Optional argument DEFAULT, a string, is the default input value, if any.
-Optional argument HISTORY, a symbol bound to a completion list (of strings)
-to offer.  It defaults to `ocv/capture-prmt-history'."
+  "prompt for string, save it to variable and insert it.
+optional argument default, a string, is the default input value, if any.
+optional argument history, a symbol bound to a completion list (of strings)
+to offer.  it defaults to `ocv/capture-prmt-history'."
   (set (make-local-variable variable)
        (let* ((history (or history 'ocv/capture-prmt-history))
               (hist-list (symbol-value history)))
@@ -134,23 +145,39 @@ to offer.  It defaults to `ocv/capture-prmt-history'."
   (interactive)
   (counsel-find-file "~/org/"))
 
+;;; stefan monnier <foo at acm.org>. it is the opposite of fill-paragraph
+
+(defun unfill-paragraph (&optional region)
+  "takes a multi-line paragraph and makes it into a single line of text."
+  (interactive (progn (barf-if-buffer-read-only) '(t)))
+  (let ((fill-column (point-max))
+        ;; this would override `fill-column' if it's an integer.
+        (emacs-lisp-docstring-fill-column t))
+    (fill-paragraph nil region)))
+
 (setq org-capture-templates
-      `(("t" "Todo" entry (file+headline ,(concat org-directory "/todo/inbox.org") "Tasks")
-         "* TODO %?")
-        ("j" "Journal" entry (file+datetree ,(concat org-directory "/private-notes/journal.org"))
+      `(("t" "todo" entry (file+headline ,(concat org-directory "/todo/inbox.org") "tasks")
+         "* todo %?")
+        ("j" "journal" entry (file+datetree ,(concat org-directory "/private-notes/journal.org"))
          "* %?")
-        ("m" "Meeting" entry (file+datetree ,(concat org-directory "/mmuldoon-work/meetings.org"))
-         "* Topic: %?\n** Type: %(ocv/prmt \"Type\" 'type nil 'ocv/capture-prmt-type-history)\n** Purpose:\n** Present:\n- Mickey\n** Notes:\n")
-        ("p" "Private meeting" entry (file+datetree ,(concat org-directory "/private-notes/meetings.org")) "* Topic: %?\n** Type: %(ocv/prmt \"Type\" 'type nil 'ocv/capture-prmt-type-history)\n** Purpose:\n** Present:\n- Mickey\n** Notes:\n")
-        ;; ("n" "Blue Note" entry
-        ;;  (file (lambda ()(concat org-directory "/blue-notes/" (read-string "Filename: "))))
-        ;;  "* %?\n %U")
+        ("m" "meeting" entry (file+datetree ,(concat org-directory "/mmuldoon-work/meetings.org"))
+         "* topic: %?\n** type: %(ocv/prmt \"type\" 'type nil 'ocv/capture-prmt-type-history)\n** purpose:\n** present:\n- mickey\n** notes:\n")
+        ("p" "private meeting" entry (file+datetree ,(concat org-directory "/private-notes/meetings.org")) "* topic: %?\n** type: %(ocv/prmt \"type\" 'type nil 'ocv/capture-prmt-type-history)\n** purpose:\n** present:\n- mickey\n** notes:\n")
+        ;; ("n" "blue note" entry
+        ;;  (file (lambda ()(concat org-directory "/blue-notes/" (read-string "filename: "))))
+        ;;  "* %?\n %u")
         ))
 
-;;; ("b" "Blue note" entry (file ,(mmuldoon/new-org-file (concat org-directory "/blue-notes/"))))
+;;; ("b" "blue note" entry (file ,(mmuldoon/new-org-file (concat org-directory "/blue-notes/"))))
 
-(require 'company-org-roam)
-(push 'company-org-roam company-backends)
+;; (require 'company-org-roam)
+(push 'company-capf company-backends)
+
+(require 'org-fc)
+(require 'org-fc-hydra)
+(global-set-key (kbd "C-c F") 'org-fc-hydra/body)
+
+(setq org-fc-directories '("~/org/aero/"))
 
 (setq org-roam-capture-templates
       `(("d" "default" plain (function org-roam-capture--get-point)
@@ -179,5 +206,29 @@ to offer.  It defaults to `ocv/capture-prmt-history'."
   (cljr-add-keybindings-with-prefix "C-c C-m"))
 
 (add-hook 'clojure-mode-hook #'my-clojure-mode-hook)
+
+;; Similar to C-x C-e, but sends to REBL
+(defun rebl-eval-last-sexp ()
+  (interactive)
+  (let* ((bounds (cider-last-sexp 'bounds))
+         (s (cider-last-sexp))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+;; Similar to C-M-x, but sends to REBL
+(defun rebl-eval-defun-at-point ()
+  (interactive)
+  (let* ((bounds (cider-defun-at-point 'bounds))
+         (s (cider-defun-at-point))
+         (reblized (concat "(cognitect.rebl/inspect " s ")")))
+    (cider-interactive-eval reblized nil bounds (cider--nrepl-print-request-map))))
+
+;; C-S-x send defun to rebl
+;; C-x C-r send last sexp to rebl (Normally bound to "find-file-read-only"... Who actually uses that though?)
+(add-hook 'cider-mode-hook
+          (lambda ()
+            (local-set-key (kbd "C-S-x") #'rebl-eval-defun-at-point)
+            (local-set-key (kbd "C-x C-r") #'rebl-eval-last-sexp)))
+
 (setq sentence-end-double-space nil)
 (require 'flycheck-clj-kondo)
